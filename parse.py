e2fd7d8b68cce13983b913c69cbbe04de90fa532
Hirofumi Hida <hirofumi.hida@gmail.com>
2019-03-13 02:29:51 +0900

    change yaml structure

--- parse.py
+++ parse.py
@@ -1,6 +1,7 @@
 import os, yaml, json, time
 from jinja2 import Environment, FileSystemLoader
 from datetime import datetime
+from datetime import timedelta
 from json import load
 
 f = open("weekly-report.yml", "r+")
@@ -8,16 +9,19 @@ data = yaml.load(f)
 
 subjectName = data[0]['name']
 reportAuther = data[0]['author']
-startDate = data[0]['start']
+startDate = data[0]['startdate']
+startDateWeekDay = data[0]['startdate'].strftime('%A')
+nextWeek = startDate + timedelta(days=7)
 sendTo = data[0]['sendto']
 sendCc = data[0]['sendcc']
 
 deliveryData = data[0]['delivery']
 trainingData = data[0]['training']
-meetingData = data[0]['meeting']
 leaveData = data[0]['leave']
 
 print(deliveryData)
+# print(trainingData)
+# print(leaveData)
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_ENVIRONMENT = Environment(
@@ -29,14 +33,13 @@ def render_template(template_filename, context):
     return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
 
 def create_weeklyreport():
-    metaData = [reportAuther, subjectName, startDate ]
+    metaData = [reportAuther, subjectName, startDate, startDateWeekDay, nextWeek ]
     mailaddrsList = [sendTo, sendCc]
     context = {
             'headerdata': metaData,
             'mailaddrs': mailaddrsList,
             'deliverydata': deliveryData,
             'trainingdata': trainingData,
-            'meetingdata': meetingData,
             'leavedata': leaveData
     }
     print(render_template('weekly-report.txt', context))
