e3d8c1b38dc85fad3176f4eca04a95418c336d38
hirofumi-hida <hirofumi.hida@microfocus.com>
2019-03-12 15:30:15 +0900

    edit files

--- parse.py
+++ parse.py
@@ -9,10 +9,20 @@ print(data[0])
 print(data[0]['name'])
 print(data[0]['author'])
 print(data[0]['sendto'])
-print(data[0]['sendcc'])
+print(data[0]['training'])
+print(data[0]['meeting'])
+print(data[0]['leave'])
 
+subjectName = data[0]['name']
+reportAuther = data[0]['author']
+startDate = data[0]['start']
 sendTo = data[0]['sendto']
 sendCc = data[0]['sendcc']
+
+deliveryData = data[0]['delivery']
+trainingData = data[0]['training']
+meetingData = data[0]['meeting']
+
 
 PATH = os.path.dirname(os.path.abspath(__file__))
 TEMPLATE_ENVIRONMENT = Environment(
@@ -24,9 +34,10 @@ def render_template(template_filename, context):
     return TEMPLATE_ENVIRONMENT.get_template(template_filename).render(context)
 
 def create_weeklyreport():
-    datalist = [sendTo, sendCc]
+    medaData = [reportAuther, subjectName, startDate ]
+    mailaddrsList = [sendTo, sendCc]
     context = {
-        'mailreceive': datalist
+        'mailaddrs': mailaddrsList
     }
     print(render_template('weekly-report.txt', context))
 
